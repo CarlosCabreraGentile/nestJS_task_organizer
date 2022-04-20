af9a8174c312a66e93aa953c7ba06d012c3a377f
Carlos Cabrera Gentile <carloscgentile@gmail.com>
2022-04-20 12:13:00 -0300

    hashed password with bcrypt

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -5,6 +5,7 @@ import {
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
 
 // the repository is the place when do the complex db interactions
 @EntityRepository(User)
@@ -12,7 +13,11 @@ export class UsersRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
-    const user = this.create({ username, password });
+    //always generate different salt, no matter if the password is the same
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
 
     try {
       await this.save(user);
