a322c4993deec8e919d7baa59fc16a4593363fa4
Carlos Cabrera Gentile <carloscgentile@gmail.com>
2022-04-21 16:07:18 -0300

    generate access token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,8 +5,16 @@ import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
   @Post('/signup')
   signup(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.authService.signUp(authCredentialsDto);
+  }
+
+  @Post('/signin')
+  signin(
+    @Body() authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    return this.authService.signIn(authCredentialsDto);
   }
 }
